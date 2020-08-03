6f8950e90c0a19070b9233dcc5725105a78fc852
jikor1st <54614268+jikor1st@users.noreply.github.com>
2020-08-04 04:53:05 +0900

    change

--- js/script.js
+++ js/script.js
@@ -103,13 +103,14 @@ minusbtn.addEventListener("click", onClickMinusBtn);
 
 function onClickRedrawBtn(e){
     e.preventDefault();
+    realDraw();
+}
+function realDraw(){
     removedraw();
-    console.log("click redraw");
     setTimeout(function(){
         ondraw();
-    },3000);
+    },1500);
 }
-
 function removedraw(){
     for(let i=0;i<horizon_line_outer.length;i++){
         horizon_line_outer[i].style.animation = "1.4s unopacityDraw 0.5s ease-in-out forwards";
@@ -144,4 +145,86 @@ function ondraw(){
         st1[i].style.animation = "4s draw ease-in-out forwards";
     }
 }
-redrawbtn.addEventListener("click", onClickRedrawBtn)
+redrawbtn.addEventListener("click", onClickRedrawBtn);
+
+const hambtn = document.querySelector(".hambtn");
+const backCard = document.querySelector(".back_card");
+const nav = document.querySelector("nav");
+const nav_link_area = document.querySelector(".nav_link_area");
+var ham = document.querySelectorAll(".ham");
+
+function onClickHamBtn(e){
+    e.preventDefault();
+    if(!ham[0].classList.contains("toggleham")){
+        realDraw();
+    }
+    backCard.classList.toggle("togglebackCard");
+    backCard.style.transition = "all 0.6s ease-in-out";
+    if(nav_link_area.classList.contains("nav_link_area")){
+        nav_link_area.classList.add("nav_scale");
+        nav_link_area.classList.remove("nav_link_area");
+        nav_link_area.style.transition = "0.2s all 0.6s ease-in-out";
+    }
+    else if(!nav_link_area.classList.contains("nav_link_area")){
+        nav_link_area.classList.remove("nav_scale");
+        nav_link_area.classList.add("nav_link_area");
+        nav_link_area.style.transition = "all 0.2s ease-in-out";
+    }
+    
+    
+    nav.classList.toggle("nav_height_remove");
+    for(let i=0;i<ham.length;i++){
+        ham[i].classList.toggle("toggleham");
+    }
+    hambtnAni();
+}
+function hambtnAni(){
+    ham[0].classList.toggle("firstham");
+    ham[1].classList.toggle("secondham");
+    ham[2].classList.toggle("thirdham");
+}
+hambtn.addEventListener("click", onClickHamBtn);
+
+window.onresize = function(e){
+    backCard.style.transition = "none";
+    nav_link_area.style.transition = "none";
+}
+
+const nav_hr = document.querySelector(".nav_link_inform hr");
+const nav_h2 = document.querySelector(".nav_inform_box h2");
+const nav_p = document.querySelector(".nav_inform_box p");
+const nav_link_inform = document.querySelector(".nav_link_inform");
+var nav_linkA = document.querySelectorAll(".nav_link > li > a");
+nav_linkA = Array.prototype.slice.call(nav_linkA);
+
+function onMouseNavA(e){
+    let mTarget = e.currentTarget;
+    let index = nav_linkA.indexOf(mTarget);
+    if(e.type === "mouseover"){
+        // console.log("over");
+        nav_link_inform.style.opacity = "1";
+        nav_hr.classList.add("informbox0" + index );
+        if(index == 0){
+            nav_h2.innerText = "기역 이란 :";
+            nav_p.innerText = "나를 소개하는 자기소개 페이지입니다.";
+        }
+        else if(index == 1){
+            nav_h2.innerText = "기역 바다 :";
+            nav_p.innerText = "바다라는 넓은 공간에 나의 프로젝트 영역을 넓혀가고 있습니다.";
+        }
+        else{
+            nav_h2.innerText = "기역 공간 :";
+            nav_p.innerText = "공간은 나의 놀이터이고, 다양한 시도를 해보는 공간입니다.";
+        }
+    }
+    else if(e.type === "mouseout"){
+        // console.log("leave");
+        nav_link_inform.style.opacity = "0";
+        nav_hr.classList.remove("informbox0" + index );
+    }
+}
+
+for(let i=0;i<nav_linkA.length;i++){
+    nav_linkA[i].addEventListener("mouseover", onMouseNavA);
+    nav_linkA[i].addEventListener("mouseout", onMouseNavA);
+}
